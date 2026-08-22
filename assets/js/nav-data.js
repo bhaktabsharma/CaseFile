1d9fbce58c827e915565995d9b46ea37408ca22c
Bhakta Sharma <onlycodingbrb003@gmail.com>
2026-08-22 10:49:50 +0530

    Change hasContent from false to true for 2FA Bypass

--- assets/js/nav-data.js
+++ assets/js/nav-data.js
@@ -6059,7 +6059,7 @@ window.SITE_DATA = {
      "slug": "pentesting-web-2fa-bypass",
      "type": "page",
      "contentPath": "content/pentesting-web/2fa-bypass.md",
-     "hasContent": false,
+     "hasContent": true,
      "children": [],
      "tag": "TAG-0650"
     },
